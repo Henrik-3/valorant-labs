e86bb1dde0c826f1bf513100adf4aca1523b5f49
Henrik-3 <43936184+Henrik-3@users.noreply.github.com>
2020-06-09 11:21:08 +0200

    Update to discord.js v12

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,12 +1,12 @@
 module.exports = async (args, client, message, { Canvas, Discord }) => {
-    
+    message.channel.startTyping()
     const canvasstats = Canvas.createCanvas(3840, 2160) //set image size
     const ctx = canvasstats.getContext('2d') //text preparation
 
-    const background = await Canvas.loadImage("https://cdn.glitch.com/6f24e132-ed6a-4704-a40d-19f2a8f508ca%2FUnbenannt-1%20(2).png?v=1588341225969"); //load background from url
+    const background = await Canvas.loadImage("commands/images/Valorant_LABS.png"); //load background from url
     ctx.drawImage(background, 0, 0, canvasstats.width, canvasstats.height); // displays background
-    
-    //function for easier text 
+  
+   //function for easier text 
     //Base: ctx.text('Text', Size, X, Y, '#Color', 'textAlign')
     ctx.text = function(content='Leer', size=100, x=0, y=0, color='#ffffff', textAlign='left') {
     ctx.font = size + 'px product_sans';
@@ -17,41 +17,58 @@ module.exports = async (args, client, message, { Canvas, Discord }) => {
   
     //function for easier text 
     //Base: ctx.text('Text', Size, X, Y, '#Color', 'textAlign')
-    ctx.text2 = function(content='Leer', size=100, x=0, y=0, color='#ffffff', textAlign='left') {
+    ctx.text2 = function(content='Leer', size=100, x=0, y=0, color='#ffffff', textAlign='left', rotate=-0.5*Math.PI) {
     ctx.font = size + 'px valorant_font';
     ctx.fillStyle = color
     ctx.textAlign = textAlign
-    ctx.fillText(content, x, y)
-    }
+    ctx.save();
+    ctx.translate(200,canvasstats.height/2);
+    ctx.rotate(rotate);
+    ctx.fillText(content , 0, 0);
+    ctx.restore();
+  }
+    //function for easier text 
+    //Base: ctx.text('Text', Size, X, Y, '#Color', 'textAlign')
+    ctx.text3 = function(content='Leer', size=100, x=0, y=0, color='#ffffff', textAlign='left') {
+    ctx.font = size + 'px valorant_font';
+    ctx.fillStyle = color
+    ctx.textAlign = textAlign
+    ctx.fillText(content , x, y);
+  }
   
     ctx.text2('Bot Overview', 180, canvasstats.width / 2, 200, '#ffffff', 'center')
-    ctx.text2('- Powering       Server', 120, 140, 500)
-    ctx.text2(client.guilds.size, 120, 975, 500, '#ff0000')
-    ctx.text2('- Created by                          and', 120, 140, 750)
-    ctx.text2('Henrik3#1451', 120, 1000, 750, '#ff0000')
-    ctx.text2('ggg#7000', 120, 2125, 750, '#ff0000')
-    ctx.text2('- Vote for us on', 120, 140, 1000)
-    ctx.text2('top.gg', 120, 1275, 1000, '#ff0000')
-    ctx.text('(v?vote)', 120, 1775, 1000)
+    ctx.text3('- Powering          Server', 120, 340, 200)
+    ctx.text3(client.guilds.cache.size, 120, 1175, 200, '#ff4654')
+    ctx.text3('- Is Online for              Users', 120, 340, 450)
+    ctx.text3(client.users.cache.size, 120, 1350, 450, '#ff4654')
+    ctx.text3('- Created by                          and', 120, 340, 700)
+    ctx.text3('Henrik3#1451', 120, 1200, 700, '#ff4654')
+    ctx.text3('ggg#7000', 120, 2325, 700, '#ff4654')
+    ctx.text3('- Design by                        , check out his', 120, 340, 950)
+    ctx.text3('ouihq#1640', 120, 1110, 950, '#ff4654')
+    ctx.text3('Reyna Bot', 120, 2910, 950, '#ff4654')
+    ctx.text3('- Vote for us on', 120, 340, 1200)
+    ctx.text3('top.gg', 120, 1475, 1200, '#ff4654')
+    ctx.text('(v?vote)', 120, 1975, 1200)
   
-    ctx.text2('Last Change:', 140, 140, 1250, '#3f888f')
-    ctx.text2('- Updated all info to v0.50', 120, 140, 1500)
-    //Text DC Tag/ID:
-    ctx.text2(message.member.user.tag, 80, 245, 150)
-    
+    ctx.text3('Last Change:', 140, 340, 1600, '#3f888f')
+    ctx.text3('- Redesign + AutoNews', 120, 340, 1850)
+    ctx.text3('- Update from Discord.js v11 to v12', 120, 340, 2100)
+
     //Avatar
-    const ctx_v = canvasstats.getContext('2d') //text preparation
-    // Pick up the pen
-	  ctx_v.beginPath();
-	  // Start the arc to form a circle
-	  ctx_v.arc(125, 125, 100, 0, Math.PI * 2, true);
-	  // Put the pen down
-	  ctx_v.closePath();
-	  // Clip off the region you drew on
-	  ctx_v.clip();
-    const avatarl = await Canvas.loadImage(message.author.displayAvatarURL);
-    ctx_v.drawImage(avatarl, 25, 25, 200, 200) 
+      // Pick up the pen
+	    ctx.beginPath();
+	    // Start the arc to form a circle
+	    ctx.arc(130, 2025, 80, 0, Math.PI * 2, true);
+	    // Put the pen down
+	    ctx.closePath();
+	    // Clip off the region you drew on
+	    ctx.clip();
   
-    const attachment = new Discord.Attachment(canvasstats.toBuffer(),"valorant-stats.png" ); //final result
+      const avatarl = await Canvas.loadImage(message.author.displayAvatarURL({ format: 'jpg'}));
+      ctx.drawImage(avatarl, 30, 1925, 200, 200)
+  
+    const attachment = new Discord.MessageAttachment(canvasstats.toBuffer(),"valorant-botinfo.png" ); //final result
     message.channel.send(attachment); //send final result
+    message.channel.stopTyping()
 }
