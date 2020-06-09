635d36b8714fb80f22f0f9681910ccb3ba1ea07d
Henrik-3 <43936184+Henrik-3@users.noreply.github.com>
2020-06-09 11:20:43 +0200

    Update to discord.js v12

--- commands/agents.js
+++ commands/agents.js
@@ -1,11 +1,12 @@
 module.exports = async (args, client, message, { Canvas, Discord }) => {
+    message.channel.startTyping()
     const canvasstats = Canvas.createCanvas(3840, 2160) //set image size
     const ctx = canvasstats.getContext('2d') //text preparation
 
-    const background = await Canvas.loadImage("https://cdn.glitch.com/6f24e132-ed6a-4704-a40d-19f2a8f508ca%2FUnbenannt-1%20(2).png?v=1588341225969"); //load background from url
+    const background = await Canvas.loadImage("./commands/images/Valorant_LABS.png"); //load background from url
     ctx.drawImage(background, 0, 0, canvasstats.width, canvasstats.height); // displays background
   
-    //function for easier text 
+   //function for easier text 
     //Base: ctx.text('Text', Size, X, Y, '#Color', 'textAlign')
     ctx.text = function(content='Leer', size=100, x=0, y=0, color='#ffffff', textAlign='left') {
     ctx.font = size + 'px product_sans';
@@ -16,12 +17,24 @@ module.exports = async (args, client, message, { Canvas, Discord }) => {
   
     //function for easier text 
     //Base: ctx.text('Text', Size, X, Y, '#Color', 'textAlign')
-    ctx.text2 = function(content='Leer', size=100, x=0, y=0, color='#ffffff', textAlign='left') {
+    ctx.text2 = function(content='Leer', size=100, x=0, y=0, color='#ffffff', textAlign='left', rotate=-0.5*Math.PI) {
     ctx.font = size + 'px valorant_font';
     ctx.fillStyle = color
     ctx.textAlign = textAlign
-    ctx.fillText(content, x, y)
-    }
+    ctx.save();
+    ctx.translate(200,canvasstats.height/2);
+    ctx.rotate(rotate);
+    ctx.fillText(content , 0, 0);
+    ctx.restore();
+  }
+    //function for easier text 
+    //Base: ctx.text('Text', Size, X, Y, '#Color', 'textAlign')
+    ctx.text3 = function(content='Leer', size=100, x=0, y=0, color='#ffffff', textAlign='left') {
+    ctx.font = size + 'px valorant_font';
+    ctx.fillStyle = color
+    ctx.textAlign = textAlign
+    ctx.fillText(content , x, y);
+  }
   
   
     const db = require('../db.js')
@@ -29,7 +42,7 @@ module.exports = async (args, client, message, { Canvas, Discord }) => {
     const Agents = {
     breach: {
       name: 'Breach',
-      url: 'https://blitz-cdn.blitz.gg/blitz/val/agents/breach/breach-cutout-compressed.png',
+      url: 'https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt100d13bfa8286a3d/5eb7cdc11ea0c32e33b95fa2/V_AGENTS_587x900_Breach.png',
       ccost: '100',
       cability: 'Aftershock',
       cabilitydescription: 'Fusion Charge through wall. The burst does heavy damage to anyone in its range',
@@ -45,7 +58,7 @@ module.exports = async (args, client, message, { Canvas, Discord }) => {
     },
     brimstone: {
       name: 'Brimstone',
-      url: 'https://blitz-cdn.blitz.gg/blitz/val/agents/brimstone/brimstone-cutout-compressed.png',
+      url: 'https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt26fcf1b5752514ee/5eb7cdbfc1dc88298d5d3799/V_AGENTS_587x900_Brimstone.png',
       ccost: '100',
       cability: 'Stim Beacon',
       cabilitydescription: 'Stim Beacon, creates on landing a field which grant players RapidFire',
@@ -61,7 +74,7 @@ module.exports = async (args, client, message, { Canvas, Discord }) => {
     },
     cypher: {
       name: 'Cypher',
-      url: 'https://blitz-cdn.blitz.gg/blitz/val/agents/cypher/cypher-cutout-compressed.png',
+      url: 'https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt158572ec37653cf3/5eb7cdc19df5cf37047009d1/V_AGENTS_587x900_Cypher.png',
       ccost: '200',
       cability: 'Trapwire',
       cabilitydescription: 'Enemy players who cross a tripwire will be tethered, and dazed after a short period if they do not destroy the device in time',
@@ -77,7 +90,7 @@ module.exports = async (args, client, message, { Canvas, Discord }) => {
     },
     jett: {
       name: 'Jett',
-      url: 'https://blitz-cdn.blitz.gg/blitz/val/agents/jett/jett-cutout2-compressed.png',
+      url: 'https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/bltceaa6cf20d328bd5/5eb7cdc1b1f2e27c950d2aaa/V_AGENTS_587x900_Jett.png',
       ccost: '100',
       cability: 'Cloudburst',
       cabilitydescription: 'Throw a projectile that expands into a brief vision-blocking cloud',
@@ -93,7 +106,7 @@ module.exports = async (args, client, message, { Canvas, Discord }) => {
     },
     omen: {
       name: 'Omen',
-      url: 'https://blitz-cdn.blitz.gg/blitz/val/agents/omen/omen-cutout-compressed.png',
+      url: 'https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt4e5af408cc7a87b5/5eb7cdc17bedc8627eff8deb/V_AGENTS_587x900_Omen.png',
       ccost: '100',
       cability: 'Shrouded Step',
       cabilitydescription: 'Shadow walk ability and see its range indicator',
@@ -109,7 +122,7 @@ module.exports = async (args, client, message, { Canvas, Discord }) => {
     },
     pheonix: {
       name: 'Pheonix',
-      url: 'https://blitz-cdn.blitz.gg/blitz/val/agents/phoenix/phoenix-cutout-compressed.png',
+      url: 'https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/bltf0200e1821b5b39f/5eb7cdc144bf8261a04d87f9/V_AGENTS_587x900_Phx.png',
       ccost: '200',
       cability: 'Blaze',
       qcost: '200',
@@ -122,7 +135,7 @@ module.exports = async (args, client, message, { Canvas, Discord }) => {
     },
     raze: {
       name: 'Raze',
-      url: 'https://blitz-cdn.blitz.gg/blitz/val/agents/raze/raze-cutout-compressed.png',
+      url: 'https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt6fef56a8182d0a81/5ebf2c2798f79d6925dbd6b4/V_AGENTS_587x900_ALL_Raze_2.png',
       ccost: '200',
       cability: 'Boom Bot',
       qcost: '200',
@@ -135,7 +148,7 @@ module.exports = async (args, client, message, { Canvas, Discord }) => {
     },
     sage: {
       name: 'Sage',
-      url: 'https://blitz-cdn.blitz.gg/blitz/val/agents/sage/sage-cutout-compressed.png',
+      url: 'https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt8a627ec10b57f4f2/5eb7cdc16509f3370a5a93b7/V_AGENTS_587x900_sage.png',
       ccost: '400',
       cability: 'Barrier Orb',
       qcost: '100',
@@ -148,7 +161,7 @@ module.exports = async (args, client, message, { Canvas, Discord }) => {
     },
     sova: {
       name: 'Sova',
-      url: 'https://blitz-cdn.blitz.gg/blitz/val/agents/sova/sova-cutout-compressed.png',
+      url: 'https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/bltf11234f4775729b7/5ebf2c275e73766852c8d5d4/V_AGENTS_587x900_ALL_Sova_2.png',
       ccost: '300',
       cability: 'Owl Drone',
       qcost: '100',
@@ -161,7 +174,7 @@ module.exports = async (args, client, message, { Canvas, Discord }) => {
     },
     viper: {
       name: 'Viper',
-      url: 'https://blitz-cdn.blitz.gg/blitz/val/agents/viper/viper-cutout-compressed.png',
+      url: 'https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/bltc825c6589eda7717/5eb7cdc6ee88132a6f6cfc25/V_AGENTS_587x900_Viper.png',
       ccost: '100',
       cability: 'Snake Bite',
       qcost: '200',
@@ -172,6 +185,21 @@ module.exports = async (args, client, message, { Canvas, Discord }) => {
       xability: 'Viper’s Pit',
       xabilitydescription: 'Creating a large cloud that reduces the vision range and maximum health of players inside of it.',
     },
+    reyna: {
+      name: 'reyna',
+      url: 'https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt6577b1f58530e6b2/5eb7cdc121a5027d77420208/V_AGENTS_587x900_Reyna.png',
+      ccost: '200',
+      cability: 'Leer',
+      qcost: '100',
+      qability: 'Devour',
+      ecost: 'Free',
+      eability: 'Dismiss',
+      eabilitydescription: 'Quake, which dazing all players in its zone',
+      xcost: '6 Ult Points',
+      xability: 'Empress',
+      xabilitydescription: 'INSTANTLY enter a frenzy, increasing firing speed, equip and reload speed.',
+    },
+
   }
     
     
@@ -182,49 +210,46 @@ module.exports = async (args, client, message, { Canvas, Discord }) => {
     const agent = Agents[name]
     
     ctx.text2('Agent: ' + agent.name, 180, canvasstats.width / 2, 200, '#ffffff', 'center')
-    ctx.text('Source: https://blitz.gg/valorant/agents', 60, 50, 2100)
       
     const agentimage = await Canvas.loadImage(agent.url); //load map from url
-    ctx.drawImage(agentimage, 3000, 350, 724, 469); // displays map
+    ctx.drawImage(agentimage, 2850, 30, 1056.6, 1620); // displays map
   
-    ctx.text2('C-Cost:', 100, 150, 450, '#3f888f')
-    ctx.text(agent.ccost, 100, 575, 450)
-    ctx.text2('C-Ability:', 100, 150, 550, '#3f888f')
-    ctx.text(agent.cability, 100, 700, 550)
+    ctx.text3('C-Cost:', 100, 350, 450, '#3f888f')
+    ctx.text(agent.ccost, 100, 800, 450)
+    ctx.text3('C-Ability:', 100, 350, 575, '#3f888f')
+    ctx.text(agent.cability, 100, 900, 575)
   
-    ctx.text2('Q-Cost:', 100, 150, 750,'#3f888f')
-    ctx.text(agent.qcost, 100, 575, 750)
-    ctx.text2('Q-Ability:', 100, 150, 850, '#3f888f')
-    ctx.text(agent.qability, 100, 700, 850)
+    ctx.text3('Q-Cost:', 100, 350, 775,'#3f888f')
+    ctx.text(agent.qcost, 100, 800, 775)
+    ctx.text3('Q-Ability:', 100, 350, 900, '#3f888f')
+    ctx.text(agent.qability, 100, 900, 900)
   
-    ctx.text2('E-Cost:', 100, 150, 1050, '#3f888f')
-    ctx.text(agent.ecost, 100, 575, 1050)
-    ctx.text2('E-Ability:', 100, 150, 1150, '#3f888f')
-    ctx.text(agent.eability, 100, 700, 1150)
+    ctx.text3('E-Cost:', 100, 350, 1100, '#3f888f')
+    ctx.text(agent.ecost, 100, 800, 1100)
+    ctx.text3('E-Ability:', 100, 350, 1225, '#3f888f')
+    ctx.text(agent.eability, 100, 900, 1225)
   
-    ctx.text2('X-Cost:', 100, 150, 1350, '#3f888f')
-    ctx.text(agent.xcost, 100, 575, 1350)
-    ctx.text2('X-Ability:', 100, 150, 1450, '#3f888f')
-    ctx.text(agent.xability, 100, 700, 1450)
-    ctx.text2('X-Ability-Description:', 100, 150, 1550, '#3f888f')
-    ctx.text(agent.xabilitydescription, 100, 150, 1650)
+    ctx.text3('X-Cost:', 100, 350, 1425, '#3f888f')
+    ctx.text(agent.xcost, 100, 800, 1425)
+    ctx.text3('X-Ability:', 100, 350, 1550, '#3f888f')
+    ctx.text(agent.xability, 100, 900, 1550)
+    ctx.text3('X-Ability-Description:', 100, 350, 1675, '#3f888f')
+    ctx.text(agent.xabilitydescription, 100, 350, 1800)
     
-    //Text DC Tag/ID:
-    ctx.text2(message.member.user.tag, 80, 245, 150)
-  
     //Avatar
-    // Pick up the pen
-	  ctx.beginPath();
-	  // Start the arc to form a circle
-	  ctx.arc(125, 125, 80, 0, Math.PI * 2, true);
-	  // Put the pen down
-	  ctx.closePath();
-	  // Clip off the region you drew on
-	  ctx.clip();
+      // Pick up the pen
+	    ctx.beginPath();
+	    // Start the arc to form a circle
+	    ctx.arc(130, 2025, 80, 0, Math.PI * 2, true);
+	    // Put the pen down
+	    ctx.closePath();
+	    // Clip off the region you drew on
+	    ctx.clip();
   
-    const avatarl = await Canvas.loadImage(message.author.displayAvatarURL);
-    ctx.drawImage(avatarl, 25, 25, 200, 200)
+      const avatarl = await Canvas.loadImage(message.author.displayAvatarURL({ format: 'jpg'}));
+      ctx.drawImage(avatarl, 30, 1925, 200, 200)
     
-    const attachment = new Discord.Attachment(canvasstats.toBuffer(),"valorant-map.png" ); //final result
+    const attachment = new Discord.MessageAttachment(canvasstats.toBuffer(),"valorant-agents.png" ); //final result
     message.channel.send(attachment); //send final result
+    message.channel.stopTyping()
 }
