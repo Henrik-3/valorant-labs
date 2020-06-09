51a9f253f5a976762a0b10adfaa78a8377614f6c
Henrik-3 <43936184+Henrik-3@users.noreply.github.com>
2020-06-09 11:27:17 +0200

    Update to discord.js v12
    message is now an embed

--- commands/vote.js
+++ commands/vote.js
@@ -1,3 +1,9 @@
 module.exports = async (args, client, message, { Canvas, Discord }) => {
-  message.channel.send('Vote for us on top.gg here: https://top.gg/bot/702201518329430117/vote')
+ const Embed = new Discord.MessageEmbed()
+	  .setColor('#ee3054')
+	  .setTitle('Vote for us on top.gg')
+	  .setURL('https://top.gg/bot/702201518329430117/vote')
+	  .setTimestamp()
+	  .setFooter('VALORANT LABS');
+	   message.channel.send(Embed);
 }
