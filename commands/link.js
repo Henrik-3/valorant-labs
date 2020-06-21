490ef0221e362316826cdf4c1cbba8ce6f93b1c7
Henrik3 <henrikmertens3@gmail.com>
2020-06-21 12:08:26 +0200

    Syntax Fixes

--- commands/link.js
+++ commands/link.js
@@ -1,16 +1,26 @@
 const fs = require('fs')
 module.exports = async (args, client, message, { Canvas, Discord }) => {
     if (!args.length) {
+      var linkjson = JSON.parse(fs.readFileSync('link.json'))
+      var author = message.author.id 
+      if(linkjson[author]) {
         var linkjson = JSON.parse(fs.readFileSync('link.json'))
-        //var linkjson = require('../link.json')
         var author = message.author.id 
         console.log(linkjson)
-    const Embed = new Discord.MessageEmbed()
-	    .setColor('#ee3054')
-	    .setTitle('Your current linked account is ' + linkjson[author].ingamename + '#' + linkjson[author].ingametag)
-	    .setTimestamp()
-	    .setFooter('VALORANT LABS');
-	    message.channel.send(Embed);
+        const Embed = new Discord.MessageEmbed()
+	        .setColor('#ee3054')
+	        .setTitle('Your current linked account is ' + linkjson[author].ingamename + '#' + linkjson[author].ingametag)
+	        .setTimestamp()
+	        .setFooter('VALORANT LABS [LINK GET]');
+        message.channel.send(Embed);
+    } else {
+        const Embed = new Discord.MessageEmbed()
+	        .setColor('#ee3054')
+	        .setTitle('You dont have a linked account, ' + message.author.username)
+	        .setTimestamp()
+	        .setFooter('VALORANT LABS [LINK UNKNOWN]');
+        message.channel.send(Embed);
+    }
     } else {
     var linkPath = './link.json'
     var linkRead = fs.readFileSync(linkPath);
@@ -24,7 +34,16 @@ module.exports = async (args, client, message, { Canvas, Discord }) => {
     const namefinal = nametest[1].split('#')
     const name = namefinal[0];
     const tag = namefinal[1];
-
+    
+    if(name == null || tag == null) {
+        const Embed = new Discord.MessageEmbed()
+            .setColor('#ee3054')
+            .setTitle('Unknown Syntax')
+            .setDescription('Please make sure you used the right syntax, as an example \n v?link HenrikX33#KEK3')
+            .setTimestamp()
+            .setFooter('VALORANT LABS [ERROR]')
+        message.channel.send(Embed)
+    } else {
     linkFile[userId] = {ingamename: name, ingametag: tag};
     fs.writeFileSync(linkPath, JSON.stringify(linkFile, null, 2));
 
@@ -32,7 +51,8 @@ module.exports = async (args, client, message, { Canvas, Discord }) => {
 	    .setColor('#ee3054')
 	    .setTitle('User succesfully linked with ' + name + '#' + tag)
 	    .setTimestamp()
-	    .setFooter('VALORANT LABS');
+	    .setFooter('VALORANT LABS [SUCCESS]');
 	    message.channel.send(Embed);
     }
+  }
 }
