a192778ed9af5bdc457652fc1ac7f44d16b5da67
Henrik Mertens <43936184+Henrik-3@users.noreply.github.com>
2021-07-25 16:33:33 +0200

    Delete commands directory

--- commands/patch.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-module.exports = async (args, client, message, { Canvas, Discord }) => {
-    const db = require('../db.js')
-    var lang = db.get(`${message.guildID}.lang`) || 'en-us'
-    var linkjson = JSON.parse(fs.readFileSync('lang.json'))
-
-    client.createMessage(message.channel.id, {embed: {title: linkjson[lang].patchtitle, color: 0xee3054, description: linkjson[lang].patchdesc, url: linkjson[lang].patchurl, image: { url: linkjson[lang].patchimage2}, timestamp: new Date().toISOString(), footer: {text: 'VALORANT LABS [PATCH]'}}})
-}
